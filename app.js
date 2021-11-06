28163983380a51521bc4f01742a0d00ac2e62f7c
ingyu hwang <dlsrb07@gmail.com>
2021-11-07 00:12:56 +0900

    init vanila

---
+++ app.js
@@ -0,0 +1,15 @@
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input")
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden"
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+    greeting.innerText = `Hello ${username}`
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
